d210f9cd04851f6b4228a895d9c82e693159079c
ian7525 <ian7525@gmail.com>
2021-09-12 01:44:39 +0800

    add Screen, Previous, Current style

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { Container } from "./Styled";
+import { Container, Current, Previous, Screen } from "./Styled";
 
 export default function Calculator() {
-  return <Container />;
+  return (
+    <Container>
+      <Screen>
+        <Previous>1</Previous>
+        <Current>2</Current>
+      </Screen>
+    </Container>
+  );
 }
